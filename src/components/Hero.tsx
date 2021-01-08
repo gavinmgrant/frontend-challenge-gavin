754e2147d794004113b32c9e309239bc0c69109d
Gavin Grant <gavingrant@gmail.com>
2021-01-08 13:30:02 -0800

    refactor(hero): fetch data from API

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 
-export default function Hero({ src }) {
-    return <Image src={src} width="792" height="373" />;
+export default function Hero() {
+    const [header, setHeader] = useState(null);
+    const [content, setContent] = useState(null);
+    const [cta, setCta] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/cms")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setHeader(result.header);
+                    setContent(result.content);
+                    setCta(result.cta);
+                }
+            )
+    }, [])
+
+    return (
+        <section>
+            <div>
+                <h1>
+                    {header}
+                </h1>
+                <p>
+                    {content}
+                </p>
+                <button>
+                    <a 
+                        href="https://twinkleintime.com/create/" 
+                        target="_blank">
+                            {cta}
+                    </a>
+                </button>
+            </div>
+            <div>
+                <Image
+                    src="/images/hero.png"
+                    alt="Star Map by Twinkle in Time"
+                    width={573}
+                    height={767}
+                />
+            </div>
+        </section>
+    );
 }
